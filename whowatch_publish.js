93266215264c2196183d4009a6031815f25ae483
sugattor <20632567+sugattor@users.noreply.github.com>
2020-03-29 07:44:15 +0900

    2020/3/29の仕様で動作するように変更、コメント追加

--- whowatch_publish.js
+++ whowatch_publish.js
@@ -1,43 +1,50 @@
-(function(){
-  let publishTime = toSecond(prompt("配信時間を入力","29:55"));
-  let nextPublish = true;
+(function () {
+  let publishTime = toSecond(prompt("配信時間を入力", "29:55"));
 
   startPublish();
   // 指定した時間になるまで経過時間を確認
-  var interval = setInterval(checkTime,1000,publishTime);
+  var interval = setInterval(checkTime, 1000, publishTime);
 
-  // 配信開始
-  function startPublish(){
-    document.getElementsByClassName('start')[0].click();
-    setTimeout(function(){document.getElementsByClassName('primary')[0].click()},200);
+  // 配信開始する関数
+  function startPublish() {
+    // 「配信する」をクリック
+    document.getElementsByClassName('primary')[0].click();
+    // 500ms後ダイアログの「配信する」をクリック
+    setTimeout(function () { document.getElementsByClassName('primary')[1].click() }, 500);
   }
 
   // 経過時間をチェックし終わらせる
-  function checkTime(stopTime){
+  function checkTime(stopTime) {
+    // 配信画面の時間(MM:SS)から配信秒数を取得する
     let time = toSecond(document.getElementsByClassName('time')[0].innerHTML);
-    console.log("time="+time);
-    if(time >= stopTime){
+    // 一応コンソールに取得した時間(秒)を表示
+    console.log("time=" + time);
+
+    // 指定時間を超えたら配信を終了させる
+    if (time >= stopTime) {
       clearInterval(interval);
       endPublish();
     }
   }
 
-  // 配信終了
-  function endPublish(){
-    document.getElementsByClassName('end')[0].click();
-    document.getElementsByClassName('dialogOk')[0].click();
-    setTimeout(function(){document.getElementsByClassName('dialog-button')[1].click()},700);
-    if(nextPublish){ // 次の配信が設定されていれば1秒後に配信開始
-      setTimeout(function(){
-        startPublish();
-        interval = setInterval(checkTime,1000,publishTime);
-      },1000);
-    }
+  // 配信終了させる関数
+  function endPublish() {
+    // 「終了する」をクリック
+    document.getElementsByClassName('warn')[0].click();
+    // 500ms後ダイアログの「終了する」をクリック
+    setTimeout(function () { document.getElementsByClassName('warn')[1].click() }, 500);
+    // 1秒後「非公開で保存する」をクリック
+    setTimeout(function () { document.getElementsByClassName('dialog-button')[1].click() }, 1000);
+    // 2秒後に配信開始
+    setTimeout(function () {
+      startPublish();
+      interval = setInterval(checkTime, 1000, publishTime);
+    }, 2000);
   }
 
-  // 時間→秒
-  function toSecond(time){
-    let [m,s] = time.split(':');
-    return m*60 + +s;
+  // 時間(MM:SS)→秒に変換する関数
+  function toSecond(time) {
+    let [m, s] = time.split(':');
+    return m * 60 + +s;
   }
 })();
