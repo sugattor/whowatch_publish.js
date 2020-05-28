ab45d1cea54693fda3e74a4cfdd0636a14c04f28
sugattor <20632567+sugattor@users.noreply.github.com>
2020-05-29 03:40:37 +0900

    Revert "配信の状態を推測して処理するように"

--- whowatch_publish.js
+++ whowatch_publish.js
@@ -1,70 +1,50 @@
 (function () {
-
   let publishTime = toSecond(prompt("配信時間を入力", "29:55"));
 
-  // 0: 開始前
-  // 1: 配信開始ダイアログが出てる
-  // 2: 配信中(指定された時間までこのまま)
-  // 3: 配信終了ダイアログが出てる
-  // 4: 保存方法のダイアログが出てる
-  let status = 0;
+  startPublish();
+  // 指定した時間になるまで経過時間を確認
+  var interval = setInterval(checkTime, 1000, publishTime);
 
-  setInterval(function(t) {
-      let liveStartDialogs;
-      let genericDialogs;
-      switch (status) {
-          case 0:
-              for (let elem of document.getElementsByTagName('button')) {
-                  if (elem.textContent === '配信する') {
-                      elem.click();
-                      status++;
-                  }
-              }
-              break;
-          case 1:
-              liveStartDialogs = document.getElementsByTagName('live-start-dialog');
-              for (let elem of liveStartDialogs[0].getElementsByTagName('button')) {
-                  if (elem.textContent === '配信する') {
-                      elem.click();
-                      status++;
-                  }
-              }
-              break;
-          case 2:
-              let time = toSecond(document.getElementsByClassName('time')[0].textContent);
-              if (time > t) {
-                  for (let elem of document.getElementsByTagName('button')) {
-                      if (elem.textContent === '終了する') {
-                          elem.click();
-                          status++;
-                      }
-                  }
-              }
-              break;
-          case 3:
-              genericDialogs = document.getElementsByTagName('generic-dialog');
-              for (let elem of genericDialogs[0].getElementsByTagName('button')) {
-                  if (elem.textContent === '終了する') {
-                      elem.click();
-                      status++;
-                  }
-              }
-              break;
-          case 4:
-              genericDialogs = document.getElementsByTagName('generic-dialog');
-              for (let elem of genericDialogs[0].getElementsByTagName('button')) {
-                  if (elem.textContent === '非公開で保存') {
-                      elem.click();
-                      status = 0;
-                  }
-              }
-              break;
-      }
-  }, 1000, publishTime);
+  // 配信開始する関数
+  function startPublish() {
+    // 「配信する」をクリック
+    document.getElementsByClassName('primary')[0].click();
+    // 500ms後ダイアログの「配信する」をクリック
+    setTimeout(function () { document.getElementsByClassName('primary')[1].click() }, 500);
+  }
+
+  // 経過時間をチェックし終わらせる
+  function checkTime(stopTime) {
+    // 配信画面の時間(MM:SS)から配信秒数を取得する
+    let time = toSecond(document.getElementsByClassName('time')[0].innerHTML);
+    // 一応コンソールに取得した時間(秒)を表示
+    console.log("time=" + time);
+
+    // 指定時間を超えたら配信を終了させる
+    if (time >= stopTime) {
+      clearInterval(interval);
+      endPublish();
+    }
+  }
+
+  // 配信終了させる関数
+  function endPublish() {
+    // 「終了する」をクリック
+    document.getElementsByClassName('warn')[0].click();
+    // 500ms後ダイアログの「終了する」をクリック
+    setTimeout(function () { document.getElementsByClassName('warn')[1].click() }, 500);
+    // 1秒後「非公開で保存する」をクリック
+    setTimeout(function () { document.getElementsByClassName('basic')[1].click() }, 1000);
+    // 2秒後に配信開始
+    setTimeout(function () {
+      startPublish();
+      interval = setInterval(checkTime, 1000, publishTime);
+    }, 2000);
+  }
 
   // 時間(MM:SS)→秒に変換する関数
   function toSecond(time) {
-      let [m, s] = time.split(':');
-      return m * 60 + +s;
+    let [m, s] = time.split(':');
+    return m * 60 + +s;
   }
 })();
